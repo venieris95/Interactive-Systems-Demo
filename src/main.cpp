e5db528ffc37f21da36b0c74b7163c396b043d73
SpyrosVen <venierisspyros@gmail.com>
2023-05-11 11:17:09 +0200

    First Commit

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(D1, OUTPUT);
+  pinMode(D0, INPUT);
+
+  digitalWrite(D1, LOW);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+    if(digitalRead(D0) == HIGH)
+    {
+      digitalWrite(D1, HIGH);
+    }
+    else
+    {
+      digitalWrite(D1, LOW);
+    }
+    
+}
